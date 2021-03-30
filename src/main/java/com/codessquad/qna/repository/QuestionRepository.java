28c479b809e95c4ae225994bf3f0313d1d94b089
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-03-30 16:19:01 +0900

    Paging 사용을 위한 Repository로 변경

--- src/main/java/com/codessquad/qna/repository/QuestionRepository.java
+++ src/main/java/com/codessquad/qna/repository/QuestionRepository.java
@@ -1,10 +1,10 @@
 package com.codessquad.qna.repository;
 
 import com.codessquad.qna.domain.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface QuestionRepository extends JpaRepository<Question, Long> {
+public interface QuestionRepository extends PagingAndSortingRepository<Question, Long> {
     List<Question> findAllByDeletedIsFalse();
 }
